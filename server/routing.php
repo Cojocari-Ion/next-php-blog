05d40d9e03c6ee57a34c7b831b8dfdf114bb64a0
Ion Cojocari <69515189+Cojocari-Ion@users.noreply.github.com>
2023-06-07 00:02:32 +0300

    asd

--- server/routing.php
+++ server/routing.php
@@ -2,22 +2,36 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', '1');
 
-require "server/services/DB.php";
+require "services/DB.php";
+require('Api.php');
+require('controllers/PostsControllers.php');
 
 use services\DB;
+use Api\Api;
 
-require('controllers/PostsControllers.php');
 
 // Getting current URL
 
 $current_link = $_SERVER['REQUEST_URI'];
 
-var_export($current_link);
-
-exit;
+if (str_contains($current_link, '?')) {
+    $current_link = explode('?', $current_link);
+}
 
 // Routes
 
 $urls = [
-    '/redux-php-blog' => ['PostsController@getPostsFromDatabase']
+    '/next-php-blog/server/posts' => ['PostsControllers@getPostsFromDatabase'],
+    '/next-php-blog/server/searchResult' => ['PostsControllers@getSearchResult']
 ];
+
+// CHeck route availability
+
+$availableRoutes = array_keys($urls);
+
+if (!in_array($current_link[0], $availableRoutes)) {
+    header('HTTP/1.0 404 NOT FOUND');
+    exit;
+}
+
+Api::routing($current_link[0], $urls);
